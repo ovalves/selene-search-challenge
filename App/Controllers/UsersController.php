c934bcb59cd73e64576efb99ff056cf04b534322
ovalves <vinicius_o.a@live.com>
2021-06-23 17:47:22 -0300

    Feat: Realizando a busca doso usuarios de acordo com parametros recebidos na request

--- App/Controllers/UsersController.php
+++ App/Controllers/UsersController.php
@@ -7,30 +7,53 @@
  */
 
 use Selene\Controllers\BaseController;
-use Selene\Render\View;
 use Selene\Request\Request;
 
 class UsersController extends BaseController
 {
-    /**
-     * Index Action.
-     */
-    public function index(Request $request): View
+    public function getUserByName(Request $request): mixed
     {
-        exit('request on controller');
-
         $params = $request->getQueryParams();
 
-        $users = $this
-            ->select('*')
-            ->table('users')
-            ->where(['title = ?' => 'matrix'])
-            ->execute()
-            ->fetchAll();
+        $name = $params['query'] ?? null;
+        $from = (int) $params['from'] ?? 0;
+        $size = (int) $params['size'] ?? 15;
 
-        /*
-        * @example render view
-        */
-        return $this->view()->render('home/home.php');
+        $users = $this
+            ->select('name')
+            ->table('users')
+            ->where(['name like ?' => "%{$name}%"])
+            ->order('id', 'ASC')
+            ->limit($size)
+            ->offset($from)
+            ->execute()
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        echo '<pre>';
+        echo json_encode($users, JSON_PRETTY_PRINT);
+        exit();
+    }
+
+    public function getUserByUserName(Request $request): mixed
+    {
+        $params = $request->getQueryParams();
+
+        $username = $params['query'] ?? null;
+        $from = (int) $params['from'] ?? 0;
+        $size = (int) $params['size'] ?? 15;
+
+        $users = $this
+            ->select('username')
+            ->table('users')
+            ->where(['username like ?' => "%{$username}%"])
+            ->order('id', 'ASC')
+            ->limit($size)
+            ->offset($from)
+            ->execute()
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        echo '<pre>';
+        echo json_encode($users, JSON_PRETTY_PRINT);
+        exit();
     }
 }
