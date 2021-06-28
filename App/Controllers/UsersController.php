aa96d52f300fb961d88fe40455a01b7e97f8e612
ovalves <vinicius_o.a@live.com>
2021-06-28 15:27:16 -0300

    Fix: Refatoração da controller, agora utilzando o gatway para executar as querys

--- App/Controllers/UsersController.php
+++ App/Controllers/UsersController.php
@@ -9,61 +9,66 @@
 use Selene\Controllers\BaseController;
 use Selene\Request\Request;
 use Selene\Response\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class UsersController extends BaseController
 {
-    public function getUserByName(Request $request, Response $response): mixed
+    private mixed $gateway = null;
+    private string $name;
+    private int $from;
+    private int $size;
+
+    public function getUserByName(Request $request, Response $response): JsonResponse
     {
-        $params = $request->getQueryParams();
+        $this->getUsersParams($request);
 
-        $name = (string) (empty($params['query'])) ? null : $params['query'];
-        $from = (int) (empty($params['from'])) ? 0 : $params['from'];
-        $size = (int) (empty($params['size'])) ? 15 : $params['size'];
+        $users = $this->getGateway()->findUsersByName(
+            $this->name,
+            $this->from,
+            $this->size
+        );
 
-        $users = $this
-            ->select([
-                'users.id',
-                'users.name',
-                'users.username',
-            ])
-            ->table('users')
-            ->leftJoin('lista_relevancia_1', 'users.id', '=', 'lista_relevancia_1.id')
-            ->leftJoin('lista_relevancia_2', 'users.id', '=', 'lista_relevancia_2.id')
-
-            ->where(['name like ?' => "%{$name}%"])
-            ->order('lista_relevancia_1.id', 'DESC')
-            ->order('lista_relevancia_2.id', 'DESC')
-            ->order('users.id', 'DESC')
-            ->limit($size)
-            ->offset($from)
-            ->execute()
-            ->fetchAll(\PDO::FETCH_ASSOC);
-
-        echo '<pre>';
-        echo json_encode($users, JSON_PRETTY_PRINT);
-        exit;
+        return $response->json([
+            'from' => (int) $this->from,
+            'size' => (int) $this->size,
+            'data' => $users,
+        ]);
     }
 
-    public function getUserByUserName(Request $request): mixed
+    public function getUserByUserName(Request $request, Response $response): JsonResponse
+    {
+        $this->getUsersParams($request);
+
+        $users = $this->getGateway()->findUsersByUserName(
+            $this->name,
+            $this->from,
+            $this->size
+        );
+
+        return $response->json([
+            'from' => (int) $this->from,
+            'size' => (int) $this->size,
+            'data' => $users,
+        ]);
+    }
+
+    private function getUsersParams(Request $request): void
     {
         $params = $request->getQueryParams();
 
-        $username = (string) (empty($params['query'])) ? null : $params['query'];
-        $from = (int) (empty($params['from'])) ? 0 : $params['from'];
-        $size = (int) (empty($params['size'])) ? 15 : $params['size'];
+        $this->name = (string) (empty($params['query'])) ? null : $params['query'];
+        $this->from = (int) (empty($params['from'])) ? 0 : $params['from'];
+        $this->size = (int) (empty($params['size'])) ? 15 : $params['size'];
+    }
 
-        $users = $this
-            ->select('username')
-            ->table('users')
-            ->where(['username like ?' => "%{$username}%"])
-            ->order('id', 'ASC')
-            ->limit($size)
-            ->offset($from)
-            ->execute()
-            ->fetchAll(\PDO::FETCH_ASSOC);
+    private function getGateway(): UsersGateway
+    {
+        if (null == $this->gateway) {
+            $this->gateway = $this->container()->set(
+                UsersGateway::class
+            );
+        }
 
-        echo '<pre>';
-        echo json_encode($users, JSON_PRETTY_PRINT);
-        exit();
+        return $this->gateway;
     }
 }
